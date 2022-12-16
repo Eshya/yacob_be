7de83b833a532f842f683f47577097b68222f434
Achmad Yaseh <achmadayas@gmail.com>
2022-12-16 19:47:50 +0700

    commit master

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+// master
 const app = express();
 
 const port = 3000;
@@ -7,11 +7,12 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send({message:"Hello Get !"});
 });
-app.get('/user', (req, res) => {
+app.post('/', (req, res) => {
+    res.send({message:"Hello Get !"});
+});
+app.delete('/user', (req, res) => {
     res.send({message:"Hello user !"});
 });
-app.get('/data', (req, res) => {
-    res.send({message:"Hello data !"});
-});
+
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
